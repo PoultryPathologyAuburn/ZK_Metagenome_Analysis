92e8d6816d4c4498279eaa294239846159668240
Zubair Khalid <90934096+Zubair2021@users.noreply.github.com>
2024-09-04 15:37:51 -0500

    Update fullkraken2_updated.sh

--- fullkraken2_updated.sh
+++ fullkraken2_updated.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-exec >> new_full_kraken.txt 2>&1
+exec >> log_kraken_bracken_run.txt 2>&1
+
+# load the module
+module load anaconda/3-2021.11 # there will be a few non-deterimental warnings with this module load 
 
 # Base directory containing nested files with .fastq.gz files
-BASE_DIR="/home/aubzxk001/host_depletion_test/trimmomatic/trimmed_reads"
+BASE_DIR="your_fastq_directory"
 
 # Target directory to store merged .fq files and output files
-TARGET_DIR="/home/aubzxk001/host_depletion_test/kraken/kraken2/kraken_full_output"
+TARGET_DIR="path_to_output"
 
 # Paths to databases and other resources
 TRUSEQ_ADAPTERS="./TruSeq3-PE.fa"
-KRAKEN_DB="/home/aubzxk001/host_depletion_test/kraken/kraken2_full"
+KRAKEN_DB="path_to_database" 
+BRACKEN_DB="path_to_database"
+
+# 1. The default standard database can be found either in 
+/datasets/unzipped/kraken2_full
+
+# 2. You can build your own custom database which can be downloaded and pre-built using 
+# https://benlangmead.github.io/aws-indexes/k2
+
+# 3. Run it from /datasets/unzipped/kraken2_full/nt_database
+# the database nt_database is huge and needs RAM management (32 cpu & 502gb RAM)
+
 
 # check if kraken db exists
 if [ ! -d "$KRAKEN_DB" ]; then
@@ -18,45 +32,82 @@ if [ ! -d "$KRAKEN_DB" ]; then
 fi
 
 
-BRACKEN_DB="/home/aubzxk001/host_depletion_test/kraken/kraken2_full"
+
 
 # Create the target directory and subdirectories if they don't exist
 mkdir -p "$TARGET_DIR"
-# mkdir -p "$TARGET_DIR/fastqc"
-# mkdir -p "$TARGET_DIR/trimmomatic"
+mkdir -p "$TARGET_DIR/trimmomatic"
 mkdir -p "$TARGET_DIR/kraken2"
 mkdir -p "$TARGET_DIR/bracken"
-# mkdir -p "$TARGET_DIR/megahit"
 
 # # echo "directories created" 
-/home/aubzxk001/host_depletion_test/kraken/kraken2/Bracken/bracken-build -d $BRACKEN_DB -t 100 -k 35
+bracken-build -d $BRACKEN_DB -t 16 -k 35
 
+
+source /apps/profiles/modules_asax.sh.dyn
+module load trimmomatic
+
+# Directory containing your FASTQ files
+input_dir="path_to_raw_reads"
+
+trimmed_reads_dir="trimmed_reads"
+
+mkdir -p "$trimmed_reads_dir"
+
+# Process each pair of FASTQ files in the directory
+for file1 in "$input_dir"/*_R1_001.fastq.gz; do
+    # Get the base name of the file
+    base_name=$(basename "$file1" _R1_001.fastq.gz)
+    echo "$base_name"
+    file2="${input_dir}/${base_name}_R2_001.fastq.gz"
+
+    if [[ -f "$file1" && -f "$file2" ]]; then
+        # Quality control and trimming using Trimmomatic
+        trimmed_file1="${trimmed_reads_dir}/${base_name}_R1.fastq.gz"
+        trimmed_file2="${trimmed_reads_dir}/${base_name}_R2.fastq.gz"
+        
+        # Run trimmomatic without unpaired reads
+        trimmomatic PE -threads 32 -phred33 \
+            "$file1" "$file2" \
+            "$trimmed_file1" /dev/null \
+            "$trimmed_file2" /dev/null \
+            ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:8:true LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
+        
+        # Check if trimming succeeded
+        if [[ $? -ne 0 ]]; then
+            echo "Trimmomatic failed for $base_name"
+            continue
+        fi
+        
+        # Count total reads in raw data
+        total_reads=$(zcat "$file1" "$file2" | wc -l)
+        total_reads=$((total_reads / 4))
+        echo "Processing sample ${base_name} with ${total_reads} total reads"
+    
+    else 
+        echo "Paired files not found for $base_name"
+    fi
+done
+
+
+# use the trimmed reads
 
 # Loop through each FASTQ file in the input directory
-for file in "$BASE_DIR"/*_R1.fastq.gz; do
+for file in "$trimmed_reads_dir"/*_R1.fastq.gz; do
       
       # Extract the sample name from the file name
       sample_name=$(basename "$file" | cut -d'_' -f1)
       echo "Processing sample: $sample_name"
 
-
-      # #fastp for quality control
-      # echo "fastp starting for $sample_name"
-      # fastp -i "$BASE_DIR/${sample_name}_R1_001.fastq.gz" -I "$BASE_DIR/${sample_name}_R2_001.fastq.gz" -o "$TARGET_DIR/fastqc/${sample_name}_R1_001_fastp.fq" -O "$TARGET_DIR/fastqc/${sample_name}_R2_001_fastp.fq" -h "$TARGET_DIR/fastqc/${sample_name}_fastp.html" -j "$TARGET_DIR/fastqc/${sample_name}_fastp.json"
-
-
-      # # Taxonomic Classification with Kraken2
-      # echo "kraken2 starting for $sample_name"
-      # kraken2 --db $KRAKEN_DB --paired "$BASE_DIR/${sample_name}_R1.fastq.gz" "$BASE_DIR/${sample_name}_R2.fastq.gz" --report "$TARGET_DIR/kraken2/${sample_name}_kraken.report" --output "$TARGET_DIR/kraken2/${sample_name}_kraken.out" --threads 32
-      # echo "kraken2 for $sample_name done"
+      # Taxonomic Classification with Kraken2
+      echo "kraken2 starting for $sample_name"
+      kraken2 --db $KRAKEN_DB --paired "$trimmed_reads_dir/${sample_name}_R1.fastq.gz" "$trimmed_reads_dir/${sample_name}_R2.fastq.gz" --report "$TARGET_DIR/kraken2/${sample_name}_kraken.report" --output "$TARGET_DIR/kraken2/${sample_name}_kraken.out" --threads 32
+      echo "kraken2 for $sample_name done"
       
-     # Estimating Abundance with Bracken
-      /home/aubzxk001/host_depletion_test/kraken/kraken2/Bracken/bracken -d $BRACKEN_DB -i "$TARGET_DIR/kraken2/${sample_name}_kraken.report" -o "$TARGET_DIR/bracken/${sample_name}_bracken.out" -r 150 -l S -t 32
+      # Estimating Abundance with Bracken
+      bracken -d $BRACKEN_DB -i "$TARGET_DIR/kraken2/${sample_name}_kraken.report" -o "$TARGET_DIR/bracken/${sample_name}_bracken.out" -r 150 -l S -t 32
       
-      
-      # # Assembly with Megahit
-      # megahit -1 "$TARGET_DIR/trimmomatic/${sample_name}_1_paired.fq" -2 "$TARGET_DIR/trimmomatic/${sample_name}_2_paired.fq" -o "${TARGET_DIR}/megahit/${sample_name}_megahit_output"
-      # echo "megahit for $file done"
+
 done
 # Remove the misplaced 'fi' statement
 echo "the script ran successfully"
